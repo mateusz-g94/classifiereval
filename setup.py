6407a7af01c077ddf852f1ef1b0c57b24a48cf15
Mateusz Grazda <mateusz.grazda@gmail.com>
2019-09-05 20:48:30 +0200

    add setup.py and __init__

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='clasifiereval',
+    version='0.0.1',
+    description='My private package.',
+    url='git@github.com:mateusz-g94/clasifiereval.git',
+    author='Mateusz Grazda',
+    author_email='mateusz.grazda@gmail.com',
+    license='unlicense',
+    packages=['clasifiereval'],
+    zip_safe=False
+)
